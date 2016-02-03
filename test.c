5968e6a1887bc9935a25800d822feda94ace06f5
vvav <whlee0303@gmail.com>
2016-02-03 11:48:13 +0900

    Add test file

---
+++ test.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void main(){
+
+	int a;
+	int b;
+	int sum;
+
+	printf("input the number1 : ");
+	scanf("%d", &a);
+	printf("input the number2 : ");
+	scanf("%d", &b);
+
+	sum = a + b;
+	printf("Sum : %d\n", sum);
+}
