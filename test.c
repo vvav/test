d9e84604c19cf40600bfdbc0f0a05f2e1f02dc40
whlee <whlee@whlee-VirtualBox.(none)>
2016-02-03 11:57:57 +0900

    Add mul value

--- test.c
+++ test.c
@@ -5,6 +5,7 @@ void main(){
 	int a;
 	int b;
 	int sum;
+	int mul;
 
 	printf("input the number1 : ");
 	scanf("%d", &a);
@@ -12,5 +13,7 @@ void main(){
 	scanf("%d", &b);
 
 	sum = a + b;
+	mul = a * b;
 	printf("Sum : %d\n", sum);
+	printf("Mul : %d\n", mul);
 }
